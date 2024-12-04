728f5c56eecafbe3b9901f3a2e22bd4f789ac534
Sabbir Tanvir <sabbirmahmudtanvir@gmail.com>
2024-12-04 22:25:11 +0600

    -m

--- paytm/apps/user-app/app/page.tsx
+++ paytm/apps/user-app/app/page.tsx
@@ -1,18 +1,11 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+import { PrismaClient } from "@repo/db/clint";
 
+const prisma = new PrismaClient();
 
 
 export default function Home() {
   return (
     <div className="text-2xl">
-      <Image
-        src="/logo.png"
-        alt="Paytm logo"
-        width={200}
-        height={200}
-      />
       hi there from user-app
     </div>
   );
