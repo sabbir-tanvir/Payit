a71ee618a49fceaa284ceb63a6047714eac76d50
Sabbir Tanvir <sabbirmahmudtanvir@gmail.com>
2024-12-05 17:01:01 +0600

    prisma

--- paytm/packages/ui/src/Appbar.tsx
+++ paytm/packages/ui/src/Appbar.tsx
@@ -15,8 +15,8 @@ export const Appbar = ({
     onSignout
 }: AppbarProps) => {
     return <div className="flex justify-between border-b px-4">
-        <div className="text-lg flex flex-col justify-center">
-            PayTM
+        <div className="text-lg  flex flex-col justify-center">
+            PayIt
         </div>
         <div className="flex flex-col justify-center pt-2">
             <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
