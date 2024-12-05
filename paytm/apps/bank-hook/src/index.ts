0215ca721e42175e1ee97b1899a3c577e2fd04ed
Sabbir Tanvir <sabbirmahmudtanvir@gmail.com>
2024-12-05 17:24:11 +0600

    sql

--- paytm/apps/bank-hook/src/index.ts
+++ paytm/apps/bank-hook/src/index.ts
@@ -3,13 +3,31 @@ import db from "@repo/db/client";
 
 const app = express();
 
-app.post("/hdfcWebhook", (req, res) => {
+app.post("/hdfcWebhook", async (req, res) => {
     //TODO: Add zod validation here?
     const paymentInformation = {
         token: req.body.token,
         userId: req.body.user_identifier,
         amount: req.body.amount
     };
-    db.
+    db.balance.update({
+        where: {
+            userId: userId
+        },
+        data: {
+            ammount: {
+                increment: paymentInformation.amount
+            }
+        }
+    });
+  await  db.onRampTracnsaction.update({
+        where: {
+            token: paymentInformation.token
+        },
+        data: {
+            status: "Success"
+        }
+    })
+    
     // Update balance in db, add txn
 })
